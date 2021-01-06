ba7591808f24f524f0b3d1939cb6594cd8ce9d12
avilatorres <2556149180101@ingenieria.usac.edu.gt>
2021-01-06 13:38:37 -0600

    Redis subscribe implemented with storage in redis server

--- redis_sub/main.go
+++ redis_sub/main.go
@@ -1,34 +1,89 @@
 package main
 
 import (
-	"context"
 	"fmt"
-  "github.com/go-redis/redis"
+	"log"
+	"time"
+	"context"
+	"encoding/json"
+
+	"github.com/gomodule/redigo/redis"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var ctx = context.Background()
+//Struct to save into Mongodb
+type Request struct {
+	Name string
+	Location string
+	Age int
+	InfectedType string `json:"infected_type"`
+	State string
+}
 
 func main() {
-  client := redis.NewClient(&redis.Options{
-		Addr:     "35.188.216.162:6379", // host:port of the redis server
-		Password: "", // no password set
-		DB:       0,  // use default DB
- 	})
+	//https://godoc.org/github.com/gomodule/redigo/redis#Pool
+	fmt.Println("Hello World")
 
-	pong, err := client.Ping(client.Context()).Result()
-
-	if err !=nil {
-		fmt.Println("err",err);
-	}
-
-	client.Set(ctx, "key", "ojala esto si funcione", 0).Err()
-
-	val, err := client.Get(ctx, "key").Result()
-
+	c, err := redis.Dial("tcp", "35.188.216.162:6379")
 	if err != nil {
-		fmt.Println("err",err);
-		panic(err)
+		log.Println(err)
+	} else {
+		log.Println("Everything is fine!!!")
 	}
+	// defer c.Close()
 
-	fmt.Println("key", val)
+	/// This code is for Subscriber
+	psc := redis.PubSubConn{Conn: c}
+	psc.Subscribe("canal1")
+	for {
+		switch v := psc.Receive().(type) {
+		case redis.Message:
+			fmt.Printf("%s: message: %s\n", v.Channel, v.Data)
+			insert_mongo(string(v.Data))
+		case redis.Subscription:
+			fmt.Printf("%s: %s %d\n", v.Channel, v.Kind, v.Count)
+		case error:
+			fmt.Println(v)
+		}
+	}
+	/// End here
+
+}
+
+// InsertMongo
+func insert_mongo(jsonString string) {
+	//JSON string (it comes from client)
+
+	//Print received data from the client
+	log.Printf("Data received: %v", jsonString)
+
+	//Connection with Mongodb Atlas
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://admin:admin123@cluster0.4d9ky.mongodb.net/testdb?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	//Accessing the collection inside the database
+	collection := client.Database("testdb").Collection("users")
+
+	//Parsing JSON string to Struct
+	var req Request	
+	json.Unmarshal([]byte(jsonString), &req)
+
+	//Checking valid struct
+	if (Request{} != req) {
+		//Inserting into Mongodb 
+		insertResult, err := collection.InsertOne(context.TODO(), req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Inserted post with ID:", insertResult.InsertedID)
+	}
 }
