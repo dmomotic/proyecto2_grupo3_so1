e22ea9257a7a6e7472185fc9e056eab9b1f088ac
Luis <salaz103@gmail.com>
2021-01-06 19:31:15 -0600

    Conexion cloud function

--- Frontend/src/components/Test.js
+++ Frontend/src/components/Test.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import axios from 'axios';
 
 class Test extends React.Component {
   state = {
+    id1:''
   };
 
   componentDidMount() {
- 
+    axios.get(`https://us-central1-g3sopes1.cloudfunctions.net/redisFunc`)
+      .then(res => {
+        const id1 = res.data;
+        console.log(res.data);
+        //this.setState({ id1 });
+      })
   }
 
   componentWillUnmount(){
